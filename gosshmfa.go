8f3fb0efd50de1b286597d61fddfc7167ee7fbd0
vinay kulkarni <kulkarnivinay621@gmail.com>
2024-08-20 23:33:09 +0530

    need to figure out how to actually write tests

--- gosshmfa.go
+++ gosshmfa.go
@@ -1,10 +1,9 @@
-package gosshmfa
-
 import (
 	"bytes"
 	"fmt"
 	"io"
 	"log"
+	"strings"
 	"syscall"
 
 	"golang.org/x/crypto/ssh"
@@ -57,37 +56,6 @@ func (conn *client) Close() error {
 	return err
 }
 
-// AuthInteractive provides a mechanism to
-// input password and MFA code ([duo app] / [yubikey] / [google authenticator app])
-//
-// [duo app]: https://duo.com/
-// [yubikey]: https://www.yubico.com/
-// [google authenticator app]: https://en.wikipedia.org/wiki/Google_Authenticator
-func AuthInteractive() ssh.AuthMethod {
-	return ssh.KeyboardInteractive(
-		func(user, instruction string, questions []string, echos []bool) ([]string, error) {
-			if len(questions) == 0 {
-				fmt.Printf("%s %s\n", user, instruction)
-			}
-			answers := make([]string, len(questions))
-			for i, question := range questions {
-				fmt.Println(question)
-				if echos[i] {
-					if _, err := fmt.Scan(&answers[i]); err != nil {
-						return nil, err
-					}
-				} else {
-					answer, err := term.ReadPassword(int(syscall.Stdin))
-					if err != nil {
-						return nil, err
-					}
-					answers[i] = string(answer)
-				}
-			}
-			return answers, nil
-		})
-}
-
 // creates a new ssh session from scratch
 func (conn *client) MakeNewSession() (*session, error) {
 	session, err := conn.sshClient.NewSession()
@@ -118,40 +86,46 @@ func (conn *session) makeNewSessionFromExistingSession() (*session, error) {
 // getting the output depending on the latency and output length,
 // checkout RunCmds if you want to run multiple commands at once
 //
-// Note: this only works on linux based servers/machines which has /bin/bash enabled as the code starts the /bin/bash enabled and executes the commands
+// Note: this only works on linux based servers/machines which has
+// /bin/bash enabled as the command is run something like this:
+//
+//	/bin/bash; <yourCmd>; exit
 func (conn *session) Run(cmd string) (*bytes.Buffer, *bytes.Buffer) {
 	defer conn.sshSession.Close()
 
+	// appending exit to the command so that the program
+	// knows that the command execution is complete,
+	// else the program will be halted forever.
+	cmd += " ;exit"
+
+	// ssh output and error
 	stdOut, stdErr := new(bytes.Buffer), new(bytes.Buffer)
-	// start bash [it is important to run any command]
-	if err := conn.sshSession.Start("/bin/bash"); err != nil {
-		log.Fatalf("failed to start bash\nerr: %s", err.Error())
-	}
 
 	// ssh output
 	sshOut, err := conn.sshSession.StdoutPipe()
 	if err != nil {
-		log.Fatalf("failed to initiate standard output pipe\nerr: %s", err.Error())
+		log.Fatalf("failed to initiate standard output pipe\n\t\terr: %s", err.Error())
 	}
 
 	// ssh error
 	sshErr, err := conn.sshSession.StderrPipe()
 	if err != nil {
-		log.Fatalf("failed to initiate error pipe\nerr: %s", err.Error())
+		log.Fatalf("failed to initiate error pipe\n\t\terr: %s", err.Error())
+	}
+
+	// start bash [it is important to before running any command] and
+	// execution of the command
+	if err := conn.sshSession.Start("/bin/bash; " + cmd); err != nil {
+		log.Fatalf("failed to start bash or execute the command: %s\n\t\terr: %s", cmd, err.Error())
 	}
 
 	// writing output to buffer
 	go io.Copy(stdOut, sshOut)
 	go io.Copy(stdErr, sshErr)
 
-	// execution of the command
-	if err := conn.sshSession.Start(cmd); err != nil {
-		log.Fatalf("failed to execute the command:%s\nerr:%s", cmd, err)
-	}
-
 	// wait for process to finish
 	if err := conn.sshSession.Wait(); err != nil {
-		log.Fatalf("failed to wait till the command execution finish, err:%s", err)
+		log.Fatalf("failed to wait till the command execution finish, err: %s", err)
 	}
 	return stdOut, stdErr
 }
@@ -160,60 +134,102 @@ func (conn *session) Run(cmd string) (*bytes.Buffer, *bytes.Buffer) {
 // every command is an entity of the slice
 // each command need to be run with a new session.
 //
-// it might not be very feasible to differentiate between the output of each command.
-// plan the set of commands you want to run at once very carefully
+// returns two maps. one for standard output of the commands executed and one if any errors occured
+//   - key -- commad name
+//   - val -- output/error
 //
 // there might be delay between execution of the command and
 // getting the output depending on the latency and output length
 //
 // Note: this only works on servers/machines which has /bin/bash as shell, the code starts the bash shell and passes the commad to shell for execution
-func (conn *session) RunCmds(cmds []string) (*bytes.Buffer, *bytes.Buffer) {
+func (conn *session) RunCmds(cmds []string) (map[string]string, map[string]string) {
 	defer conn.sshSession.Close()
 
-	stdOut, stdErr := new(bytes.Buffer), new(bytes.Buffer)
+	// the output and error map
+	stdOutMap, stdErrMap := make(map[string]string), make(map[string]string)
 
-	for _, cmd := range cmds {
+	for i, cmd := range cmds {
+
+		// exiting from the shell after executing the last command
+		if i == len(cmds)-1 {
+			cmd += ";exit"
+		}
+
+		// making a new client through which new sessions will be made
 		currClient, err := conn.makeNewSessionFromExistingSession()
 		if err != nil {
-			log.Fatalf("problem in creating a new session from an existing session\nerr: %s", err.Error())
-		}
-		// start bash [it is important to run any command]
-		if err := currClient.sshSession.Start("/bin/bash"); err != nil {
-			log.Fatalf("failed to start bash \nerr: %s", err.Error())
+			log.Fatalf("problem in creating a new session from an existing session\n\t\terr: %s", err.Error())
 		}
 
 		// ssh output
 		sshOut, err := currClient.sshSession.StdoutPipe()
 		if err != nil {
-			log.Fatalf("failed to initiate standard output pipe\nerr: %s", err.Error())
+			log.Fatalf("failed to initiate standard output pipe\n\t\terr: %s", err.Error())
 		}
 
 		// ssh error
 		sshErr, err := currClient.sshSession.StderrPipe()
 		if err != nil {
-			log.Fatalf("failed to initiate error pipe\nerr: %s", err.Error())
+			log.Fatalf("failed to initiate error pipe\n\t\terr: %s", err.Error())
 		}
 
-		// writing output to buffer
-		go io.Copy(stdOut, sshOut)
-		go io.Copy(stdErr, sshErr)
-
-		// execution of the command
-		if err := currClient.sshSession.Start(cmd); err != nil {
-			log.Fatalf("failed to execute the command:%s\nerr:%s", cmd, err.Error())
+		// start bash [it is important to run any command]
+		// and execute the command
+		if err := currClient.sshSession.Start("/bin/bash; " + cmd); err != nil {
+			log.Printf("failed to execute /bin/bash or command: %s\n\t\terr: %s", cmd, err.Error())
 		}
+
+		// making a buffer where you can write the data to
+		stdOutBuff, stdErrBuff := new(bytes.Buffer), new(bytes.Buffer)
+		go io.Copy(stdOutBuff, sshOut)
+		go io.Copy(stdErrBuff, sshErr)
 
 		// wait for process to finish
 		if err := currClient.sshSession.Wait(); err != nil {
-			log.Fatalf("failed to wait till the command execution finish, err:%s", err)
+			log.Printf("there was an error while executing a command\n\t\t last executed command: %s\n\t\terr: %s", cmd, err.Error())
 		}
+
+		// map
+		stdOutMap[strings.Replace(cmd, ";exit", "", 1)] = stdOutBuff.String()
+		stdErrMap[strings.Replace(cmd, ";exit", "", 1)] = stdErrBuff.String()
 	}
-	return stdOut, stdErr
+	return stdOutMap, stdErrMap
+}
+
+// AuthInteractive provides a mechanism to
+// input password and MFA code ([duo app] / [yubikey] / [google authenticator app])
+//
+// [duo app]: https://duo.com/
+// [yubikey]: https://www.yubico.com/
+// [google authenticator app]: https://en.wikipedia.org/wiki/Google_Authenticator
+func authInteractive() ssh.AuthMethod {
+	return ssh.KeyboardInteractive(
+		func(user, instruction string, questions []string, echos []bool) ([]string, error) {
+			if len(questions) == 0 {
+				fmt.Printf("%s %s\n", user, instruction)
+			}
+			answers := make([]string, len(questions))
+			for i, question := range questions {
+				fmt.Println(question)
+				if echos[i] {
+					if _, err := fmt.Scan(&answers[i]); err != nil {
+						return nil, err
+					}
+				} else {
+					answer, err := term.ReadPassword(int(syscall.Stdin))
+					if err != nil {
+						return nil, err
+					}
+					answers[i] = string(answer)
+				}
+			}
+			return answers, nil
+		})
 }
 
 // create an SSH client
 func MakeSSHConnection(sshConf *SSHConfig) (*client, error) {
-	authMehtod := AuthInteractive()
+	authMehtod := authInteractive()
 	config := &ssh.ClientConfig{
 		User: sshConf.User,
 		Auth: []ssh.AuthMethod{
